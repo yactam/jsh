7a258316bbf2c46b48dd5e82d2d5a1cbb15a98b5
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-11-26 00:12:38 +0100

    global variables to store last return value and last working directory

---
+++ headers/global_variables.h
@@ -0,0 +1,13 @@
+#ifndef __GLOBAL_VARIABLES_H__
+#define __GLOBAL_VARIABLES_H__
+#define PATH_MAXSIZE 1024
+
+extern int last_return;
+extern char oldwd[PATH_MAXSIZE]; 
+
+extern void setReturn(int);
+extern void setOldwd(char*);
+extern int getReturn();
+extern char* getOldwd();
+
+#endif
---
+++ src/global_variables.c
@@ -0,0 +1,24 @@
+#include "../headers/global_variables.h"
+#include <stdio.h>
+#include <string.h>
+
+int last_return = 0;
+char oldwd[PATH_MAXSIZE] = {0};
+
+void setReturn(int val) {
+    last_return = val;
+}
+
+int getReturn() {
+    return last_return;
+}
+
+void setOldwd(char *wd) {
+    strncpy(oldwd, wd, PATH_MAXSIZE - 1);
+}
+
+char *getOldwd() {
+    if (oldwd[0] == 0)
+        return NULL;
+    return oldwd;
+}
