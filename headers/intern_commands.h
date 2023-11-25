ebf2ab3146bc572c4eb49279b96f8dc7533e7822
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-11-26 00:13:18 +0100

    implement some intern commands

---
+++ headers/intern_commands.h
@@ -0,0 +1,24 @@
+#ifndef __INTERN_COMMANDS_H__
+#define __INTERN_COMMANDS_H__
+
+#include <stdlib.h>
+#include <sys/types.h>
+
+int pwd();
+int cd_path(char*);
+int cd();
+int cd_();
+void jsh_exit();
+void jsh_exit_val(int);
+int jobs();
+int jobs_num(unsigned);
+int job_t();
+int job_t_num(unsigned);
+int bg(unsigned);
+int fg(unsigned);
+int kill_job(unsigned);
+int kill_job_sig(int, unsigned);
+int kill_process(pid_t);
+int kill_process_sig(int, pid_t);
+
+#endif
---
+++ src/intern_commands.c
@@ -0,0 +1,18 @@
+#include "../headers/intern_commands.h"
+#include "../headers/debug.h"
+#include "../headers/global_variables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void jsh_exit() {
+    jsh_exit_val(getReturn());
+}
+
+void jsh_exit_val(int val) {
+    fflush(stdout);
+    fflush(stderr);
+    exit(val);
+}
